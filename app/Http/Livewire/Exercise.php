4220dbc06bf2ac3835274e9ac5b755024df68eb3
Tyler Williams <tylerkmw@gmail.com>
2021-02-25 15:34:07 -0500

    Change property names to camel case

--- app/Http/Livewire/Exercise.php
+++ app/Http/Livewire/Exercise.php
@@ -10,14 +10,14 @@ class Exercise extends Component
 {
 
     public $exercise;
-    public $reps_today;
-    public $reps_in_set;
-    public $time_since_last_set;
+    public $repsToday;
+    public $repsInSet;
+    public $timeSinceLastSet;
 
     public function mount()
     {
         // Store the reps_in_set from the exericse model in public property for wire:model binding
-        $this->reps_in_set = $this->exercise->reps_in_set;
+        $this->repsInSet = $this->exercise->reps_in_set;
 
         $this->getRepsToday();
         $this->getTimeSinceLastSet();
@@ -31,20 +31,19 @@ class Exercise extends Component
     public function getTimeSinceLastSet()
     {
         $set = $this->exercise->sets()->latest()->first();
-        $this->time_since_last_set = $set->created_at->diffForHumans();
+        $this->timeSinceLastSet = $set->created_at->diffForHumans();
     }
 
     public function getRepsToday()
     {
-        $date = new \DateTime('today');
-        $this->reps_today = $this->exercise->sets()->whereDate('created_at', Carbon::today())->sum('reps');
+        $this->repsToday = $this->exercise->sets()->whereDate('created_at', Carbon::today())->sum('reps');
     }
 
     public function updatedRepsInSet()
     {
         // Update the reps_in_set for this exercise if the inputted value is not null
-        if(!empty($this->reps_in_set)) {
-            $this->exercise->update(['reps_in_set' => $this->reps_in_set]);
+        if(!empty($this->repsInSet)) {
+            $this->exercise->update(['reps_in_set' => $this->repsInSet]);
         }
     }
 
@@ -52,7 +51,7 @@ class Exercise extends Component
     {
         Set::create([
             'exercise_id' => $this->exercise->id,
-            'reps' => $this->reps_in_set
+            'reps' => $this->repsInSet
         ]);
 
         $this->getRepsToday();
--- resources/views/livewire/exercise.blade.php
+++ resources/views/livewire/exercise.blade.php
@@ -1,9 +1,9 @@
 <div>
     <div class="bg-gray-200 p-5 w-1/3 m-5">
         <h1>{{ $exercise->name }}</h1>
-        <h1 class="font-bold">Reps Today: {{ $reps_today }}</h1>
-        <h1 wire:poll.10000ms="getTimeSinceLastSet">Last set {{ $time_since_last_set }}</h1>
-        <input type="number" wire:model="reps_in_set">
+        <h1 class="font-bold">Reps Today: {{ $repsToday }}</h1>
+        <h1 wire:poll.10000ms="getTimeSinceLastSet">Last set {{ $timeSinceLastSet }}</h1>
+        <input type="number" wire:model="repsInSet">
         <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="addSet()">
             + Set
         </button>
